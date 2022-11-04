9aadc1b88d0053f9c1cf14b8a7c093e4dc214a98
zyh <onesuperman2019@outlook.com>
2022-11-04 16:56:11 +0800

    commit start

--- src/main/java/com/sensor/common/config/Swagger3Config.java
+++ src/main/java/com/sensor/common/config/Swagger3Config.java
@@ -2,8 +2,12 @@ package com.sensor.common.config;
 
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 /**
  * @author apple
@@ -15,10 +19,10 @@ import io.swagger.v3.oas.annotations.info.Info;
                 description = "Swagger接口文档",
                 contact = @Contact(name = "zach")
         ),
-   //     security = @SecurityRequirement(name = "JWT"),
+        security = @SecurityRequirement(name = "JWT"),
         externalDocs = @ExternalDocumentation(description = "参考文档",
                 url = "https://github.com/swagger-api/swagger-core/wiki/Swagger-2.X---Annotations"
         )
 )
-// @SecurityScheme(type = SecuritySchemeType.HTTP, name = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER)
+@SecurityScheme(type = SecuritySchemeType.HTTP, name = "JWT", scheme = "bearer", in = SecuritySchemeIn.HEADER)
 public class Swagger3Config {}
